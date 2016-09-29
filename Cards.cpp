b6a44982e3ad8a7c5657334be86c14c4f7b26a56
Chien Nguyen <cmnguyen999@yahoo.com>
2016-09-29 13:21:18 -0500

    Made values to the cards

--- Cards.cpp
+++ Cards.cpp
@@ -1,5 +1,49 @@
+#include <ctime>
+#include <cstdlib>
 #include "Cards.h"
 
+// give cards values
 Cards::Cards(){
-  
+  for(int i = 0; i < 13; i++){
+    if(i > 9){
+      hearts[i] = 10;
+    }else{
+      hearts[i] = i+1;
+    }
+  }
+  for(int i = 0; i < 13; i++){
+    if(i > 9){
+      diamonds[i] = 10;
+    }else{
+      diamonds[i] = i+1;
+    }
+  }
+  for(int i = 0; i < 13; i++){
+    if(i > 9){
+      spades[i] = 10;
+    }else{
+      spades[i] = i+1;
+    }
+  }
+  for(int i = 0; i < 13; i++){
+    if(i > 9){
+      clubs[i] = 10;
+    }else{
+      clubs[i] = i+1;
+    }
+  }
+}
+// returns random cards
+int Cards::CardsDealt(){
+
+  return 0;
+}
+// tests if cards has actual value
+void Cards::CardsValue(){
+  for(int i = 0; i < 13; i++){
+    std::cout << hearts[i] << "-hearts ";
+    std::cout << diamonds[i] << "-diamonds ";
+    std::cout << spades[i] << "-spades ";
+    std::cout << clubs[i] << "-clubs\n";
+  }
 }
--- Cards.h
+++ Cards.h
@@ -5,8 +5,14 @@
 class Cards{
 public:
   Cards();
+  int CardsDealt();
+  void CardsValue();
 private:
-
+  int hearts[13];
+  int diamonds[13];
+  int spades[13];
+  int clubs[13];
+  int deck[52];
 };
 
 #endif
