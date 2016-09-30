a4d6179d1d93ff54d141cf41c828527f16b62fcc
Chien Nguyen <cmnguyen999@yahoo.com>
2016-09-29 23:05:07 -0500

    Begin with 2 cards

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,23 @@
 #include "Player.h"
 
 Player::Player(){
-
+  playerValue = 0;
+  cardOne = 0;
+  cardTwo = 0;
+}
+// Automatic player gets 2 cards
+int Player::DefaultPlayerCards(){
+  cardOne = cards.CardsDealt();
+  cardTwo = cards.CardsDealt();
+  if((cardOne == 1 && cardTwo == 10) || (cardOne == 10 && cardTwo == 1)){
+    return playerValue;
+  }else{
+    if(cardOne == 1){
+      return playerValue = 11 + cardTwo;
+    }else if(cardTwo == 1){
+      return playerValue = cardOne + 11;
+    }else{
+      return playerValue = cardOne + cardTwo;
+    }
+  }
 }
--- Player.h
+++ Player.h
@@ -1,12 +1,17 @@
 #ifndef Player_h
 #define Player_h
 #include <iostream>
+#include "Cards.h"
 
 class Player{
 public:
   Player();
+  int DefaultPlayerCards();
 private:
-
+  int cardOne;
+  int cardTwo;
+  int playerValue;
+  Cards cards;
 };
 
 #endif
