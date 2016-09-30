9811dc8bdbf01b648b2810c3f935fd463dc588f9
Chien Nguyen <cmnguyen999@yahoo.com>
2016-09-29 23:04:51 -0500

    Begin with 2 cards

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,5 +1,23 @@
 #include "Dealer.h"
 
 Dealer::Dealer(){
-  
+  dealerValue = 0;
+  cardOne = 0;
+  cardTwo = 0;
+}
+
+int Dealer::DefaultDealerCards(){
+  cardOne = cards.CardsDealt();
+  cardTwo = cards.CardsDealt();
+  if((cardOne == 1 && cardTwo == 10) || (cardOne == 10 && cardTwo == 1)){
+    return dealerValue;
+  }else{
+    if(cardOne == 1){
+      return dealerValue = 11 + cardTwo;
+    }else if(cardTwo == 1){
+      return dealerValue = cardTwo + 11;
+    }else{
+      return dealerValue = cardOne + cardTwo;
+    }
+  }
 }
--- Dealer.h
+++ Dealer.h
@@ -1,12 +1,17 @@
 #ifndef Dealer_h
 #define Dealer_h
 #include <iostream>
+#include "Cards.h"
 
 class Dealer{
 public:
   Dealer();
+  int DefaultDealerCards();
 private:
-
+  int cardOne;
+  int cardTwo;
+  int dealerValue;
+  Cards cards;
 };
 
 #endif
